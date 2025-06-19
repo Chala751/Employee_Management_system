73fe1e6772953980e75609465df287365c5c6f3c
Chala Temesgen <ctemesgen85@gmail.com>
2025-06-19 04:57:12 -0300

    creating new employe

---
+++ app/employees/new/page.tsx
@@ -0,0 +1,10 @@
+import EmployeeForm from '@/components/EmployeeForm'
+
+export default function NewEmployeePage() {
+  return (
+    <div className="p-6">
+      <h2 className="text-xl font-bold mb-4">New Employee</h2>
+      <EmployeeForm />
+    </div>
+  )
+}
